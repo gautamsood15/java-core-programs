10f705d3c4adb8461d423380443b3bdcb9acb3a2
gautam sood <gauty22@gmail.com>
2018-01-04 23:19:53 +0530

    Add files via upload

---
+++ DoubleArr.java
@@ -0,0 +1,12 @@
+public class DoubleArr
+{
+	public static void main(String[] args){
+		int [][]arr = new int[4][];
+		arr[0] = new int[2];
+		arr[1] = new int[3];
+		arr[2] = new int[4];
+		arr[3] = new int[5];
+		arr[4] = new int[6];
+		System.out.println(arr[1].length);
+	}
+}
