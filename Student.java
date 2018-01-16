4901534859a239081e06b07fbf023ca71054875e
gautam sood <gauty22@gmail.com>
2018-01-16 19:22:42 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,20 @@
+package pack1;
+public class Student
+{
+		 int rollno;
+		 String name;
+		 void setName(String n){ 
+			 name=n; 
+			 }
+		 void setRollno(int r){
+			 rollno=r; 
+			 }
+		 int getRollno(){ 
+			 return(rollno); 
+			 }
+		 String getName(){ 
+			 return(name); 
+			 }
+	
+	
+}
