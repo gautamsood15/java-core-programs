071695d26f64e7b8b4f21ee0d50a7fae4e3f0980
gautam sood <gauty22@gmail.com>
2018-01-16 19:23:37 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,16 @@
+class A
+{
+	public void fxn();
+}
+
+public  class Test2
+{
+	public  static void main(String[] agrs){
+		
+		A:fxn();{
+			System.out.println("the function is working");
+		}
+	A obj = new A();
+	obj.fxn();
+	
+	}}
